cafae10b7825e2fdae9da066f550ab5368039cc3
Rattanarit Prasomsab <rattanarit.prasomsab@scbtechx.io>
2025-06-30 00:17:42 +0700

    answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def main():
+def preprocess_data(df):
     # Read the CSV file into a DataFrame
     df = pd.read_csv('data/data.csv')
 
@@ -8,6 +8,29 @@ def main():
     print(df.head())
 
     print(df.isnull().sum())
+
+    # filter year >= 2023 delete it
+    df = df[df['year'] < 2023]
+
+    # If gender is null or empty -> replace it with ‘Male’
+    df['gender'] = df['gender'].fillna('Male')
+
+    # if age ​​is null/empty/na -> replaced with the ‘mean’ value of age. (Round up)
+    average_age = df['age'].mean()
+    average_age = round(average_age)
+    print(f"Average age: {average_age}")
+    df['age'] = df['age'].fillna(average_age)
+
+    # save the modified DataFrame to a new CSV file
+    df.to_csv('data/transformed_data.csv', index=False)
+    print(df.isnull().sum())
+
+def main():
+    # preprocess_data()
+    df = pd.read_csv('data/transformed_data.csv')
+    print(df.head())
+
+    
 
 if __name__ == "__main__":
     main()
